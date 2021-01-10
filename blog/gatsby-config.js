6863d38bd382a8df65097ebeeda283751ccb3d3a
haeguri <haegyun821@gmail.com>
2021-01-11 00:14:29 +0900

    feat: date formatString 옵션 추가

--- blog/gatsby-config.js
+++ blog/gatsby-config.js
@@ -14,6 +14,7 @@ module.exports = {
       resolve: `@lekoarts/gatsby-theme-minimal-blog`,
       // See the theme's README for all available options
       options: {
+        formatString: "YYYY.MM.DD",
         navigation: [
           {
             title: `Blog`,
