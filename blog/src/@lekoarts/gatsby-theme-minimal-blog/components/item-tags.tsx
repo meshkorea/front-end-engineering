11855609e9b9d7c1be31f23af96c960ac36cb41a
haeguri <haegyun821@gmail.com>
2021-01-14 18:28:37 +0900

    feat: 태그 항목을 링크로 래핑

--- blog/src/@lekoarts/gatsby-theme-minimal-blog/components/item-tags.tsx
+++ blog/src/@lekoarts/gatsby-theme-minimal-blog/components/item-tags.tsx
@@ -1,6 +1,9 @@
 /** @jsx jsx */
 import { jsx } from "theme-ui";
 import React from "react"
+import { Link } from "gatsby";
+import replaceSlashes from "@lekoarts/gatsby-theme-minimal-blog/src/utils/replaceSlashes";
+import useMinimalBlogConfig from "@lekoarts/gatsby-theme-minimal-blog/src/hooks/use-minimal-blog-config";
 
 import { Tag } from "../../../types";
 
@@ -9,6 +12,8 @@ type TagsProps = {
 }
 
 const ItemTags = ({ tags }: TagsProps) => {
+  const { tagsPath, basePath } = useMinimalBlogConfig()
+
   const tagDelimiter = " ";
   const tagPrefix = "#";
   return (
@@ -16,11 +21,12 @@ const ItemTags = ({ tags }: TagsProps) => {
       {tags.map((tag, i) => (
         <React.Fragment key={tag.slug}>
           {!!i && tagDelimiter}
-          <span sx={{
-            opacity: "0.7",
-          }}>
+          <Link 
+            sx={{opacity: "0.7", textDecoration: "none"}} 
+            to={replaceSlashes(`/${basePath}/${tagsPath}/${tag.slug}`)}
+          >
             {`${tagPrefix}${tag.name}`}
-          </span>
+          </Link>
         </React.Fragment>
       ))}
     </React.Fragment>
