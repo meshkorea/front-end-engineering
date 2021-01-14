a95e536c72c65f9b670ccd09fc0b3b9727ba2bfa
haeguri <haegyun821@gmail.com>
2021-01-14 14:59:33 +0900

    fix: 불필요한 커스텀 필드 제거

--- blog/gatsby-node.js
+++ blog/gatsby-node.js
@@ -1,18 +1,12 @@
-exports.onCreateNode = ({ node, actions, getNode, createNodeId, createContentDigest }, themeOptions) => {
-  const { createNode, createNodeField } = actions;
+exports.onCreateNode = ({ node }) => {
   if (node.internal.type === "MarkdownRemark") {
     let {
       slug: frontmatterSlug,
     } = node.frontmatter;
 
     if (!frontmatterSlug) {
-      throw new Error("'slug' is required in frontmatter.")
+      throw new Error("'slug' field is required in frontmatter.")
     }
-
-    createNodeField({
-      node,
-      name: "slug",
-      value: frontmatterSlug,
-    });
   }
 }
+
